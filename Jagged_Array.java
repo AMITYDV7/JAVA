747b1c9f1e924fe1f12b2e181bcf65bf89a73551
AMITYDV7 <amit5yadav5762@gmai.com>
2023-07-29 20:39:22 +0530

    Some changes in jagged file

--- Jagged_Array.java
+++ Jagged_Array.java
@@ -10,7 +10,18 @@ int num[][]= new int[3][];
 num[0]= new int[4];
 num[1]= new int[3];
 num[2]= new int[2];
-num[0][1]=2;
+num[0][0]=2;
+num[0][1]=3;
+num[0][2]=4;
+num[0][3]=5;
+num[1][0]=6;
+num[1][1]=7;
+num[1][2]=8;
+num[2][0]=9;
+num[2][1]=10;
+
+
+
 
  for(int i=0;i<=2;i++)
     {
