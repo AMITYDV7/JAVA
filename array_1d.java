f10e603e93139be3778ec63e1427b5202d33d1a7
AMITYDV7 <amit5yadav5762@gmai.com>
2023-07-29 14:11:38 +0530

    add 1 d array file

--- array_1d.java
+++ array_1d.java
@@ -1,7 +1,20 @@
 public class array_1d {
     public static void main(String[] args) {
-        int a[]={2,3,4,5};
-        System.out.println(a[1]);
+        // int a[]={2,3,4,5};
+        // // System.out.println(a[0]);
+        // // System.out.println(a[1]);
+        // // System.out.println(a[2]);
+        // // System.out.println(a[3]);
+        // for(int i=0;i<=3;i++){
+        //     System.out.print(a[i]);
+        // }
+
+        int num[] = new int[4];
+        num[0] = 3;
+      
+      for(int i=0;i<=3;i++){
+        System.out.println(num[i]);
+      }
     }
     
 }
