90521fceb9293138bc43ea62383d15467a613848
AMITYDV7 <amit5yadav5762@gmai.com>
2023-07-30 21:48:35 +0530

    add some more array in for each loop

--- for_each_loop.java
+++ for_each_loop.java
@@ -1,12 +1,34 @@
 public class for_each_loop 
 {
     // it is very important loop for array because in jagged array the row and column are not same
-    public static void main(String args[]){
-    int nums[] = {2,3,4,5};
-
-    for(int n : nums)
+    public static void main(String args[])
     {
-        System.out.println(n);
-          }
-}
-}
+    // int nums[] = {2,3,4,5};
+
+    // for(int n : nums)
+    // {
+    //     System.out.println(n);
+    //       }
+    int nums[][]={
+    {2,3,4,5},
+    {6,7,8},
+    {9,0}
+    };
+                
+    for(int a[] : nums)
+    {
+        for(int b: a )
+        // length is use to know the index value of an arrays
+        {
+            System.out.print(b +" ");
+        }
+        System.out.println( );
+    }
+
+
+   }
+ }
+    
+
+
+
