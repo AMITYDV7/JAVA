73240c9430c8addc59af1d5ca3c2cb24b7071fe5
AMITYDV7 <amit5yadav5762@gmai.com>
2023-07-30 11:50:09 +0530

    Add some array files

---
+++ enhanced_for_loop.java
@@ -0,0 +1,8 @@
+public class enhanced_for_loop {
+    public static void main(String args[]){
+        // when can use enhanced for loop 
+        int num[]={2,4,6,8};
+        System.out.println(num[3]);
+    }
+}
+
