dc077af76cb66079f1658412b5e58e9b703dfbc9
AMITYDV7 <amit5yadav5762@gmai.com>
2023-07-30 21:30:25 +0530

    add enhanced loop file

--- for_each_loop.java
+++ for_each_loop.java
@@ -1,3 +1,12 @@
-public class for_each_loop {
+public class for_each_loop 
+{
     // it is very important loop for array because in jagged array the row and column are not same
+    public static void main(String args[]){
+    int nums[] = {2,3,4,5};
+
+    for(int n : nums)
+    {
+        System.out.println(n);
+          }
+}
 }
