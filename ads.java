f11e6771dab1f1504dc0679a7ae309d620df1f29
AMITYDV7 <amit5yadav5762@gmai.com>
2023-07-30 17:11:59 +0530

    add some change in the ads java file

--- ads.java
+++ ads.java
@@ -1,5 +1,8 @@
 public class ads {
     public static void main(String args[]){
 System.out.println("Amit yadav");
+System.out.println("Amit yadav");
+System.out.println("Amit yadav");
+System.out.println("Amit yadav");
     }
 }
