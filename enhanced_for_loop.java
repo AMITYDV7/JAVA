c0d57f25fd15c62f39e14c44af80801089784257
AMITYDV7 <amit5yadav5762@gmai.com>
2023-07-30 11:12:28 +0530

    add for each loop/ enhance loop file

---
+++ enhanced_for_loop.java
@@ -0,0 +1,7 @@
+public class enhanced_for_loop {
+    public static void main(String args[]){
+        // when can use enhanced for loop 
+        int num[]={2,4,6,8};
+        System.out.println(num[3]);
+    }
+}
