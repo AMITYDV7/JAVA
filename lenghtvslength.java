e4e8788f348f99a985425d41d5bae809664a7cc1
AMITYDV7 <amit5yadav5762@gmai.com>
2023-07-31 11:23:09 +0530

    add length file

--- lenghtvslength.java
+++ lenghtvslength.java
@@ -1,7 +1,14 @@
 public class lenghtvslength {
     public static void main(String args[]){
 
-        int a[]= {3,5,7,9};
-        System.out.println(a.length);
+        // int a[]= {3,5,7,9};
+        // System.out.println(a.length);
+        int nums[][] = {
+            {3,5,67,45},
+            {2,5,7},
+            {2,7}
+        };
+
+        }
     }
-}
+
