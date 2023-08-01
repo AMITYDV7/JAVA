238b107a2a73e731c269cc11ae87840ced8ab936
Amit <amit5yadav5762@gmail.com>
2023-08-01 21:51:28 +0530

    adding examples of arrays

--- arrayindexoutofboundexception.java
+++ arrayindexoutofboundexception.java
@@ -1,5 +1,15 @@
 public class arrayindexoutofboundexception {
     public static void main(String[] args) {
-        
+        int a[]={2,5,67,90};
+        // System.out.println(a[a.length-1]);
+        // System.out.println("amit");
+        // if we want to use for loop 
+        // for(int i=0;i<4;i++){ // we should not use <= in this use only <
+        //     System.out.println(a[i]);
+        // }
+        // we can use enhance for loop instead of this
+        for(int n:a){
+            System.out.println(n);
+        }
     }
 }
