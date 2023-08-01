20b59fe9f67b3aea7c7de6bc6862a1ffa790a09f
Amit <amit5yadav5762@gmail.com>
2023-08-01 21:03:38 +0530

    add example of length for 1d and 2d array

--- lenghtvslength.java
+++ lenghtvslength.java
@@ -1,29 +1,17 @@
 public class lenghtvslength {
     public static void main(String args[]){
 
-        // int a[]= {3,5,7,9};
-        // System.out.println(a.length);
-        // int nums[][] = {
-        //     {3,5,67,45},
-        //     {2,5,7},
-        //     {2,7}
-        // };
-        // int nums2[][] = {
-        //     {3,5,67,45},
-        //     {2,5,8},
-        //     {2,7}
-        // };
-        // int nums1[][] = {
-        //     {3,5,67,45},
-        //     {2,5,8},
-        //     {2,7}
-        // };
+        int a[]= {3,5,7,9};
+        System.out.println(a.length);
+        int nums[][] = {
+            {3,5,67,45},
+            {2,5,7},
+            {2,7}
+        };
+        System.out.println(nums[2].length);
+        
 
-int a[]={2,34,5};
-System.out.println(a[2]);
-int b[]={2,34,5};
-System.out.println(b[2]);
-int c[] ={3,5,6,7};
+
         }
     }
 
