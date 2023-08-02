a26802e2b7089296c87d88d24b775285e2292bd6
Amit <amit5yadav5762@gmail.com>
2023-08-02 16:01:15 +0530

    add object file in it

--- anonymous_array.java
+++ anonymous_array.java
@@ -1,14 +1,21 @@
-
-
-
-
 class calc{
-    public 
+    public int add(int nums[]){
+        int result =0;
+        for(int n :nums){
+            result = result +n;
+     
+        }
+        return result; 
+    }
 }
 
 
 public class anonymous_array {
-    public static void main(String[] args) {
-        System.out.println("1244");
+    public static void main(String[] args) 
+    {
+        calc obj = new calc();
+        int nums[]={5,6,8,9};
+        int result = obj.add(nums);
+        System.out.println(result);
     }
 }
