d71313cccb90006b49a0b75f23d39e722d978112
Amit <amit5yadav5762@gmail.com>
2023-08-20 18:17:26 +0530

    adding code to my addition of two number file

--- SumOfTwoNumber.java
+++ SumOfTwoNumber.java
@@ -1,3 +1,25 @@
-public class SumOfTwoNumber {
+ 
+ import java.util.Scanner;
+ class addition{
+    void add(){
+Scanner scan = new Scanner(System.in);
+System.out.print("Enter your Number:");
+int num1 = scan.nextInt();
+
+System.out.print("Enter your second number:");
+int num2 = scan.nextInt();
+
+int result = num1 + num2;
+System.out.println("Addition of your number: "+result);
+System.out.print("Congratulation your program have zero Error");
+    }
+ } 
+ class SumOfTwoNumber{
+    public static void main(String[] args) {
+        addition a = new addition();
+        a.add(); 
     
-}
+    }
+ }
+    
+
