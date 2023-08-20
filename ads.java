c8d1f9cc9affb65b91ccc91cc348cfdc930351b0
Amit <amit5yadav5762@gmail.com>
2023-08-20 14:09:52 +0530

    add new file of adding two number

--- ads.java
+++
@@ -1,9 +0,0 @@
-public class ads {
-    public static void main(String args[]){
-System.out.println("Amit yadav");
-System.out.println("Amit yadav");
-System.out.println("Amit yadav");
-System.out.println("Amit yadav");
-System.out.println("Amit yadav");
-    }
-}
