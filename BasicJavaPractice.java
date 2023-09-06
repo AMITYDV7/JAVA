aa2036fcab2f17d885405a0303a50ddfee24f397
Amit <amit5yadav5762@gmail.com>
2023-09-06 22:29:02 +0530

    Solving 7 question of exercise

--- BasicJavaPractice.java
+++ BasicJavaPractice.java
@@ -1,3 +1,46 @@
-public class BasicJavaPractice {
-    
+import java.util.Scanner;
+public class BasicJavaPractice 
+{
+    // This is the question <<<----number 6--------> of exercise
+    /*  public static void main(String[] args) 
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter your number:");
+        int a = scan.nextInt();
+
+         System.out.print("Enter your number:");
+        int b = scan.nextInt();
+
+      //  int c = a+b,d= a-b, e = a*b, f = a/b ,g=  a%b;
+// System.out.println("Sum of this number is:"+ c);
+// System.out.println("Subtraction of this number is:"+ d);
+// System.out.println("Multiplication of this number is:"+ e);
+// System.out.println("Division  of this number is:"+ f);
+// System.out.println("Reminder of this number is:"+ g);
+
+// new approach for this question ------->>>>
+System.out.println("Sum of this number is:"+ (a+b));
+System.out.println("Subtraction of this number is:"+ (a-b));
+System.out.println("Multiplication of this number is:"+ (a*b));
+System.out.println("Division  of this number is:"+ (a/b));
+System.out.println("Reminder of this number is:"+ (a%b));
+
+    } */
+
+
+    //         <<<<------ Question number 7 ------>>>>
+  /*  public static void main(String[] args) 
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter your number");
+        int a = scan.nextInt();
+        for(int i=1;i<=10;i++){
+            System.out.println(a + "X" + i + "=" + (a*i));
+       
+        }
+        scan.close();
+    } */
+
+
+    //  <<<<----- Question 8 --------> 
 }
