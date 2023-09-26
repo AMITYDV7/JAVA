753b8d3d2332bde1b5dd5a3793d6372ae4896e69
Amit <amit5yadav5762@gmail.com>
2023-09-26 12:24:02 +0530

    create value typed and reference typed assignment

---
+++ vtaVsRta.java
@@ -0,0 +1,21 @@
+public class vtaVsRta {
+    
+        public static void main(String[] args) {
+            //Example of value typed assignment
+            // int a=23;
+            // int b;
+            //  b=a;
+            //  System.out.println(a);
+            //  System.out.println(b);
+    
+            //  b= 45;
+            //  System.out.println(a);
+            //  System.out.println(b);
+
+            //Example of reference typed assignment
+            
+
+
+
+    }
+}
