513403562b4d4d6ba507d5459b80787de8f6292c
Amit <amit5yadav5762@gmail.com>
2023-09-27 12:25:09 +0530

    Adding code of Question 1

--- ConditionQuestionPractice.java
+++ ConditionQuestionPractice.java
@@ -1,3 +1,44 @@
-public class ConditionQuestionPractice {
-    
+import java.util.Scanner;
+public class ConditionQuestionPractice 
+{
+    // Question 1 ) Write a program to find out the enter number is positive or negative
+    /*public static void main(String args[])
+  {
+Scanner scan = new Scanner(System.in);
+
+System.out.print("Enter your number:");
+int num1 = scan.nextInt();
+
+if(num1>0){
+    System.out.print("Your number is positive");
 }
+else if(num1<0){
+    System.out.println("Your number is negative");
+}
+else{
+    System.out.println("Your number is Zero");
+}
+scan.close();
+  } */
+
+
+  // Question 2) WAP to finding roots of quadratic equation !
+        public static void main(String[] args) {
+
+            Scanner scan = new Scanner(System.in);
+System.out.print("Enter the value of a");
+int num1= scan.nextInt();
+
+System.out.print("Enter the value of b");
+int num2= scan.nextInt();
+
+System.out.print("Enter the value of c");
+int num3= scan.nextInt();
+
+if()
+
+
+            scan.close();
+        }
+    }
+
