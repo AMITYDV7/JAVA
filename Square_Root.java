e85edfca4718e89f25398449f82e97a93c9bc183
Amit <amit5yadav5762@gmail.com>
2023-09-30 19:45:18 +0530

    changing name of Square.java to Square_root.java
    in this program i approach to method for finding Square root of any number by multiple it to 0.5 or we can use Math.sqrt  method

--- Square_Root.java
+++ Square_Root.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 // import java.util.*;
 
 
-public class Square {
+public class Square_Root {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
