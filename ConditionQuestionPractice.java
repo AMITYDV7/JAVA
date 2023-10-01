5b25cfd55bf6a6f84fcb7a0c7104169b12e2d352
Amit <amit5yadav5762@gmail.com>
2023-10-01 21:29:21 +0530

    solving 2 more question of  condition statement

--- ConditionQuestionPractice.java
+++ ConditionQuestionPractice.java
@@ -1,6 +1,6 @@
-import java.util.Scanner;
-public class ConditionQuestionPractice 
-{
+// import java.util.Scanner;
+// public class ConditionQuestionPractice 
+// {
     // Question 1 ) Write a program to find out the enter number is positive or negative
     /*public static void main(String args[])
   {
@@ -23,22 +23,68 @@ scan.close();
 
 
   // Question 2) WAP to finding roots of quadratic equation !
-        public static void main(String[] args) {
+      /*   public static void main(String[] args) {
 
             Scanner scan = new Scanner(System.in);
-System.out.print("Enter the value of a");
-int num1= scan.nextInt();
+System.out.print("Enter the value of a: ");
+double a= scan.nextDouble();
 
-System.out.print("Enter the value of b");
-int num2= scan.nextInt();
+System.out.print("Enter the value of b: ");
+double b= scan.nextDouble();
 
-System.out.print("Enter the value of c");
-int num3= scan.nextInt();
+System.out.print("Enter the value of c: ");
+double c= scan.nextDouble();
 
-if()
+double d = (b*b) - 4*a*c;
+System.out.println("The deteriment of this equation is: " + d);
 
-
+if(d>0){
+    double r1 = (-b + Math.sqrt(d)) /( 2*a);
+    double r2 = (-b - Math.sqrt(d)) /( 2*a);
+    System.out.println("The root of this equation is " + r1 + "and" + r2);
+}
+else if (d==0){
+    System.out.println("Your root are real");
+    double R1 =  -b/(2*a);
+    double R2 =  -b/(2*a);
+    System.out.println("Your root are "+ R1 + R2);
+}
+else{
+    System.out.println("Your Equation has no real roots");
+}
             scan.close();
         }
     }
+*/
 
+// <<<<---------- Question ==3 ----------->>>>>>>>>>>>>>>
+//Compare three number and tell wqhich is greatest
+
+import java.util.Scanner;
+class ConditionQuestionPractice{
+    public static void main(String[] args) {
+       
+        Scanner sc = new Scanner(System.in);
+System.out.println("enter the value of a , b and c");
+int a = sc.nextInt();
+int b = sc.nextInt();
+int c = sc.nextInt();
+
+if(a>b){
+   if(a>c){
+        System.out.println("A is greatest");
+   }
+    }
+if(b>a){
+    if(b>c){
+       System.out.println("B is greatest");
+    }
+}
+else{
+    System.out.println("C is greatest");
+}
+
+
+sc.close();
+    }
+}
