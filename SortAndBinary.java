171550a11e2b774074985b68c7a9ed4235521eda
Amit <amit5yadav5762@gmail.com>
2023-10-21 17:12:41 +0530

    creating other java file for sort

--- SortAndBinary.java
+++ SortAndBinary.java
@@ -1,3 +1,11 @@
+import java.util.Arrays;
 public class SortAndBinary {
+    public static void main(String[] args) {
+        
     
+    int []a = {5,6,4,2,1,8,6,9};
+    int []b = Arrays.copyOf(a,a.length);
+    Arrays.sort(b);
+    System.out.println(Arrays.toString(b));
+}
 }
