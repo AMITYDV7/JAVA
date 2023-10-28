abd36d8e4bfef826b4299817b0b3854928aa8de4
Amit <amit5yadav5762@gmail.com>
2023-10-28 21:44:49 +0530

    add more string

--- ConcatMethodOfString.java
+++ ConcatMethodOfString.java
@@ -2,7 +2,7 @@ public class ConcatMethodOfString {
     public static void main(String[] args) {
         String s1 = "Amit";
         String s2 = "Yadav";
-        s1.concat(s2);
-        System.out.println();
+    String s3= s1.concat(s2);
+        System.out.println(s3);
     }
 }
