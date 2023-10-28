f26d59de28b6894645719aacf3cba6d18ee7c795
Amit <amit5yadav5762@gmail.com>
2023-10-28 21:33:18 +0530

    adding two string s1,s2 to preform concatenation

--- ConcatMethodOfString.java
+++ ConcatMethodOfString.java
@@ -1,5 +1,8 @@
 public class ConcatMethodOfString {
     public static void main(String[] args) {
-        
+        String s1 = "Amit";
+        String s2 = "Yadav";
+        s1.concat(s2);
+        System.out.println();
     }
 }
