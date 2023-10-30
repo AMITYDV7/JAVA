e4bab8cb382fa01634094bcfc4ed9cd0c249173f
Amit <amit5yadav5762@gmail.com>
2023-10-30 21:53:25 +0530

    Using eguals operator

--- string.java
+++ string.java
@@ -1,6 +1,9 @@
 class string{
     public static void main(String[] args) {
-       
+       String s1 = new String("Amit");
+       String s2 = new String("Amit");
+
+       System.out.println(s1==s2);
     }
     
 }
