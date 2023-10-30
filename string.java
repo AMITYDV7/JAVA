8c4225a01264b69e906a52527e8350978b03cd06
Amit <amit5yadav5762@gmail.com>
2023-10-30 21:55:09 +0530

    adding code for equals method

--- string.java
+++ string.java
@@ -1,9 +1,10 @@
 class string{
     public static void main(String[] args) {
        String s1 = new String("Amit");
-       String s2 = new String("Amit");
+       String s2 = new String("amit");
 
-       System.out.println(s1==s2);
+    //    System.out.println(s1==s2);
+    System.out.println(s1.equals(s2));
     }
     
 }
