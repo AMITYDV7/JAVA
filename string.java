707bd3eab909874d5848860fd873b73211cbccc3
Amit <amit5yadav5762@gmail.com>
2023-10-31 21:18:40 +0530

    adding code of charAt() method of string

--- string.java
+++ string.java
@@ -1,11 +1,15 @@
 class string{
     public static void main(String[] args) {
        String s1 = new String("Amit");
-       String s2 = new String("amit");
+     //  String s2 = new String("amit");
 
     //    System.out.println(s1==s2);
     // System.out.println(s1.equals(s2));
-    System.out.println(s1.equalsIgnoreCase(s2));
+    // System.out.println(s1.equalsIgnoreCase(s2));
+    int index1 = s1.charAt(2);
+
+    System.out.println((char)index1);
+
     }
     
 }
