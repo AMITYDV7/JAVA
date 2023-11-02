fdc64f8d079818680cbd3d6d2691081fbb5da034
Amit <amit5yadav5762@gmail.com>
2023-11-02 21:15:27 +0530

    add code of indexOf method of string

--- StringInbuildMethod.java
+++ StringInbuildMethod.java
@@ -1,5 +1,9 @@
 public class StringInbuildMethod {
     public static void main(String[] args) {
+        String s = "RajaRamMohanRoy";
+        System.out.println(s.charAt(6));
+        System.out.println(s.indexOf('z'));
+        System.out.println(s.indexOf('R',2));
         
     }
 }
