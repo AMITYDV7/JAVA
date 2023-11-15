df42a7c3e15f348cd84fa38c93a8e647fd9d89ba
Amit <amit5yadav5762@gmail.com>
2023-11-15 13:50:02 +0530

    adding code of encapsulation

---
+++ Encapsulation.java
@@ -0,0 +1,13 @@
+
+class book{
+    int page;
+}
+
+public class Encapsulation {
+    public static void main(String []args){
+book b = new book();
+b.page= 34;
+System.out.print(b.page);
+
+    }
+}
