8502b1d2cc2e7fc0887d85d0e12bd519780b7b9c
Amit <amit5yadav5762@gmail.com>
2023-11-15 20:40:03 +0530

    adding encapsulation code in simple code

--- Encapsulation.java
+++ Encapsulation.java
@@ -1,13 +1,35 @@
 
 class book{
-    int page;
+ private  int page; // This is data member in encapsulation . but inreal time they are instance variable
+
+ public void  setData(int x)
+ {
+if(x>0){
+    page = x;
 }
+else{
+    System.out.println("invalid number");
+}
+}
+public int getData(){
+    if(page>0){
+        return page;
+    }
+    else{
+        System.out.println("Book is empty");
+        return 0;
+    }
+ }
+}
+
 
 public class Encapsulation {
     public static void main(String []args){
 book b = new book();
-b.page= 34;
-System.out.print(b.page);
+// b.page= 34;
+// System.out.print(b.page);
+b.setData(0);
+System.out.println(b.getData());
 
     }
 }
