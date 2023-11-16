f46f989c433e619c84c8bc292b53adbc29f18121
Amit <amit5yadav5762@gmail.com>
2023-11-16 21:48:22 +0530

    solving shadowing problem

--- Encapsulation.java
+++ Encapsulation.java
@@ -1,35 +1,71 @@
 
-class book{
- private  int page; // This is data member in encapsulation . but inreal time they are instance variable
+// class book{
+//  private  int page; // This is data member in encapsulation . but inreal time they are instance variable
 
- public void  setData(int x)
- {
-if(x>0){
-    page = x;
-}
-else{
-    System.out.println("invalid number");
-}
-}
-public int getData(){
-    if(page>0){
-        return page;
+//  public void  setData(int x)
+//  {
+// if(x>0){
+//     page = x;
+// }
+// else{
+//     System.out.println("invalid number");
+// }
+// }
+// public int getData(){
+//     if(page>0){
+//         return page;
+//     }
+//     else{
+//         System.out.println("Book is empty");
+//         return 0;
+//     }
+//  }
+// }
+
+
+// public class Encapsulation {
+//     public static void main(String []args){
+// book b = new book();
+// // b.page= 34;
+// // System.out.print(b.page);
+// b.setData(0);
+// System.out.println(b.getData());
+
+//     }
+// }
+
+
+
+// Next example for this keyword
+class Dog{
+    private String breed;
+    private double age;
+    private int price;
+
+    public void setDog(String breed,double age,int price){
+    //   breed = breed;
+    //   age = age; // this variable faces shadowing problem to prevent shadowing problem we have to use this keyword
+    //    price = price;
     }
-    else{
-        System.out.println("Book is empty");
-        return 0;
+    public String getBreed(){
+        return breed;
     }
- }
+    public double getAge(){
+        return age;
+    }
+    public int getPrice(){
+        return price;
+    }
 }
 
+class Encapsulation{
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.setDog("AMit", 345, 245);
 
-public class Encapsulation {
-    public static void main(String []args){
-book b = new book();
-// b.page= 34;
-// System.out.print(b.page);
-b.setData(0);
-System.out.println(b.getData());
+        System.out.println(d.getBreed());
+        System.out.println(d.getAge());
+        System.out.println(d.getPrice());
 
     }
 }
