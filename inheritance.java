2a573a253e608b18103fbb70b9e38497fbc5b1c0
Amit <amit5yadav5762@gmail.com>
2023-11-28 19:00:39 +0530

    adding code of single inheritance in this file

---
+++ inheritance.java
@@ -0,0 +1,21 @@
+// example of single inheriatance in java 
+class demo1{
+    public void dog1(){
+        System.out.println("dog is sleeping");
+    }
+}
+
+
+class demo2 extends demo1{
+    public void dog2(){
+        System.out.println("dog is playing");
+    }
+}
+class inheritance{
+    public static void main(String[] args) {
+        demo2 rox = new demo2();
+
+        rox.dog2();
+        rox.dog1();
+    }
+}
