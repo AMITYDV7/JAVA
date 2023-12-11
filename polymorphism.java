10d8212b8c6862c75311ef3ca4730d83c3f77cf5
Amit <amit5yadav5762@gmail.com>
2023-12-11 21:38:05 +0530

    creating example of polymorphism

--- polymorphism.java
+++ polymorphism.java
@@ -1,3 +1,39 @@
-public class polymorphism {
-    
+public class plane{
+    public void fly(){
+        System.out.print("plane can fly");
+    }
+    public void run(){
+        System.out.print("plane can run");
+
+    }
 }
+
+ public class cargoplane extends plane {
+public void fly(){
+    System.out.println("plane can fly at low high");
+}
+public void run(){
+    System.out.println("plane can run slow");
+}
+ }
+
+ public class passplane extends plane {
+public void fly(){
+    System.out.println("plane can fly at medium high");
+}
+public void run(){
+    System.out.println("plane can run medium");
+}
+ }
+
+ public class fighterplane extends plane {
+public void fly(){
+    System.out.println("plane can fly at very low high");
+}
+public void run(){
+    System.out.println("plane can run fast");
+}
+ }
+
+
+ 
