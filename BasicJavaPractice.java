94396dcf929742fbdfa9e116ed6a682beebe3dff
Amit <amit5yadav5762@gmail.com>
2023-12-22 16:30:03 +0530

    exploring type casting

--- BasicJavaPractice.java
+++ BasicJavaPractice.java
@@ -1,7 +1,24 @@
 class BasicJavaPractice{
   public static void main(String []args){
-    System.out.println("Hello");
-    System.out.print("Amit Yadav");
-    
+    char a = 'A';
+ 
+    int b;  // this is a example of type casting 
+    b = a;
+
+    System.out.println(b);
+  
+  // There is also second approach in java by using in built method = Charter.getNumericValue(parameter)
+  // here is the example for this
+  char c ='A';
+  int d = Character.getNumericValue(c);
+  System.out.print(d);
+
+  char myChar = 'A'; // Example character
+int asciiValue = Character.getNumericValue(myChar); // Get ASCII value of 'A'
+System.out.println("The ASCII value of '" + myChar + "' is " + asciiValue);
+
+
+
+
   }
 }
