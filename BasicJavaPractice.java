62cd087c3121e217f392bd633320cb758900929c
Amit <amit5yadav5762@gmail.com>
2023-12-23 16:11:25 +0530

    practise some print Question

--- BasicJavaPractice.java
+++ BasicJavaPractice.java
@@ -1,4 +1,4 @@
-/* 
+ 
 class BasicJavaPractice{
   public static void main(String []args){
     char a = '`';
@@ -24,11 +24,37 @@ int v = (int)x;
 System.out.println(x);
 System.out.println(v);
 
-  }
-} */
+// Question one 
+System.out.print("Hey there\n");
+System.out.println("I am data");
 
-class BasicJavaPractice{
-  public static void main(String[] args) {
-    System.out.println((int)'A');// this is exlicit type casting
+//Question 2nd
+
   }
-}
+} 
+
+// Another approach for type casting
+
+// class BasicJavaPractice{
+//   public static void main(String[] args) {
+//     System.out.println((int)'A');// this is exlicit type casting
+//   }
+// }  
+
+
+
+// input from user
+//inport java.util.Scanner;
+// class BasicJavaPractice{
+//   public static void main(String[] args) {
+
+//     Scanner s = new Scanner(System.in);
+
+//   System.out.print("Enter your number= ");
+//   int num = s.nextInt();
+//   int num1 = s.nextInt();
+
+//   System.out.print("Your number is "+num + " "+ num1);
+    
+//   }
+// }
