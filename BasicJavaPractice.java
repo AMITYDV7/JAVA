a6a1688cd58e946531050a0d87abf36aa2ee4762
Amit <amit5yadav5762@gmail.com>
2023-12-23 09:45:23 +0530

    Type casting
    We can perform type casting in print statement. This type casting is but this type casting is explicit type casing

--- BasicJavaPractice.java
+++ BasicJavaPractice.java
@@ -1,24 +1,34 @@
-// class BasicJavaPractice{
-//   public static void main(String []args){
-//     char a = '`';
+/* 
+class BasicJavaPractice{
+  public static void main(String []args){
+    char a = '`';
  
-//    int b;  // this is a example of type casting 
-//     b = a;
+   int b;  // this is a example of type casting 
+    b = a;
 
-//      System.out.println(b);
+     System.out.println(b);
   
-//   // There is also second approach in java by using in built method = Charter.getNumericValue(parameter)
-//   // here is the example for this
-//   char c ='P';
-//   int d = Character.getNumericValue(c);
-//   System.out.print(d);
+  // There is also second approach in java by using in built method = Charter.getNumericValue(parameter)
+  // here is the example for this
+  char c ='P';
+  int d = Character.getNumericValue(c);
+  System.out.print(d);
 
-// //   char myChar = 'A'; // Example character
-// // int asciiValue = Character.getNumericValue(myChar); // Get ASCII value of 'A'
-// // System.out.println("The ASCII value of '" + myChar + "' is " + asciiValue);
-
-
+//   char myChar = 'A'; // Example character
+// int asciiValue = Character.getNumericValue(myChar); // Get ASCII value of 'A'
+// System.out.println("The ASCII value of '" + myChar + "' is " + asciiValue);
 
 
-//   }
-// }
+char x= 'g';
+int v = (int)x;
+System.out.println(x);
+System.out.println(v);
+
+  }
+} */
+
+class BasicJavaPractice{
+  public static void main(String[] args) {
+    System.out.println((int)'A');
+  }
+}
