eb6c16df4c582ee133db8f8402f9e7a97ca63e63
Amit <amit5yadav5762@gmail.com>
2023-12-23 21:20:25 +0530

    Solving question of know Data type

---
+++ knowdatatype.java
@@ -0,0 +1,26 @@
+public class knowdatatype {
+    public static void main(String[] args) {
+        int a = 4;
+        double b= 3.345;
+        char c = 'A';
+        System.out.println("a = "+ a +" b = "+ b+" c = "+c);
+
+        int area = 2*3;
+        System.out.println("Area of rectangle is "+ area);
+
+        System.out.println(8.2*6);
+
+        System.out.println((int)'h');
+double d = 34.543523;
+        System.out.println((int)d);
+
+      int v = 3+'d';
+      System.out.println((char)v);
+
+      System.out.println(5+6.2);
+
+      System.out.println(3.9*3.9);
+
+      System.out.println(5+8+16+10+11+3+8+6+9+6+11+5+10+7+11+3+5+6+1+9+7);
+    }
+}
