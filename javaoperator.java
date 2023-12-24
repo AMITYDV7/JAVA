60caace618b1aa082494887f996766479b8c7294
Amit <amit5yadav5762@gmail.com>
2023-12-24 21:10:30 +0530

    Practicing Java operators

---
+++ javaoperator.java
@@ -0,0 +1,18 @@
+public class javaoperator {
+    public static void main(String[] args) {
+
+int l=5,b=7;
+int area = l*b,perimeter = 2*(l+b);
+
+System.out.println("Area is"+area  +"perimter is"+perimeter);
+
+        System.out.println("Perimeter of triangle is "+ (2+3+5));
+
+        System.out.println((((8+2345)/3)%5)*5);
+
+        int number;
+        number = 2345;
+        number += 8;
+        
+    }
+}
