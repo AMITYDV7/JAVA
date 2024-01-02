4377298a80b81b185dbd1c4d973edc5a84a3dee6
Amit <amit5yadav5762@gmail.com>
2024-01-02 13:17:20 +0530

    learning new concept that nextInt() method cannot consume full line we have to add new method like  nextLine() method to consume a full line
    learning new concept that nextInt() method cannot consume full line we have to add new method like  nextLine() method to consume a full line

--- inputbyuser.java
+++ inputbyuser.java
@@ -1,27 +1,51 @@
-import java.util.Scanner;
-public class inputbyuser {
-    public static void main(String []args){
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter your 1st number ");
-        int num1 = sc.nextInt();
+// import java.util.Scanner;
+// public class inputbyuser {
+//     public static void main(String []args){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter your 1st number ");
+//         int num1 = sc.nextInt();
 
-        System.out.print("Enter your 2nd number ");
-        int num2 = sc.nextInt();
+//         System.out.print("Enter your 2nd number ");
+//         int num2 = sc.nextInt();
 
-        System.out.println("Your sum of these number is "+ (num1+num2));
+//         System.out.println("Your sum of these number is "+ (num1+num2));
 
-        System.out.println("Your product of these number is "+ (num1*num2));
-        sc.close(); 
+//         System.out.println("Your product of these number is "+ (num1*num2));
+//         sc.close(); 
 
-        // que 3 area of rectangle related question 
+//         // que 3 area of rectangle related question 
         
-        System.out.println("Enter length ");
-        double l = sc.nextDouble();
+//         System.out.println("Enter length ");
+//         double l = sc.nextDouble();
         
-        System.out.println("ENter your breadth ");
-        double b = sc.nextDouble();
+//         System.out.println("ENter your breadth ");
+//         double b = sc.nextDouble();
 
-        System.out.println("Area of rectangle is " + (l*b));
-    }
+//         System.out.println("Area of rectangle is " + (l*b));
+//     }
     
+// }
+
+import java.util.Scanner;
+class inputbyuser{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your name:");
+        String name = sc.nextLine();
+
+        System.out.println("Enter your Roll number:");
+        int rollnu = sc.nextInt();
+
+        sc.nextLine();////To consume '\n' left by ENTER KEY, it is not consumed by nextInt()
+
+        System.out.print("Enter your field of interest:");
+        // we can also add consume line there 
+        String field = sc.nextLine();
+       
+
+        System.out.println("My name is "+ name + " My roll number is " + rollnu + " My field of interest is " + field);
+        sc.close();
+    }
+
 }
