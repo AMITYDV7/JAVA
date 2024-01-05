4b8c9f9d5af2a381740277bcec470372873cf032
Amit <amit5yadav5762@gmail.com>
2024-01-05 21:40:01 +0530

    question 10

--- iforelse.java
+++ iforelse.java
@@ -253,6 +253,22 @@ import javax.sound.midi.Soundbank;
 //     }
 // }
 
+// Question 10
+// class iforelse{
+//   public static void main(String[] args){
+//     int x = 2;
+//     int y = 5;
+//     int z = 0;
+//     System.out.println(x==2);
+//     System.out.println(x != 5);
+//     System.out.println(x != 5 && y >= 5);
+//     System.out.println(z != 0 || x == 2);
+//     System.out.println(!(y < 10));
+//   }
+// }
+
+// Question 11
+
 
 
 
