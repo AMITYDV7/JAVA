77c109eff57593571ea2a026147f69729a060af9
Amit <amit5yadav5762@gmail.com>
2024-01-08 20:59:59 +0530

    Question 2 of loop

--- looplooploop.java
+++ looplooploop.java
@@ -1,19 +1,43 @@
-import java.util.Scanner;
+//Question 1
 
-class looplooploop{
-    public static void main(String[] args) {
-     for(int i = 0;i<5;i=i+1){
-        for(int j =0;j<5;j=j+1){
-            System.out.print("3");
-        }
-        System.out.println("");
-     }
+// class looplooploop{
+//     public static void main(String[] args) {
+//      for(int i = 0;i<5;i=i+1){
+//         for(int j =0;j<5;j=j+1){
+//             System.out.print("3");
+//         }
+//         System.out.println("");
+//      }
         
-    }
-}
+//     }
+// }
 // Output is 
-33333
-33333
-33333
-33333
-33333
+// 33333
+// 33333
+// 33333
+// 33333
+// 33333
+
+// Question ==2
+// class looplooploop{
+//     public static void main(String []args){
+//         int n =5;
+
+//         for(int i=0;i<n;i=i+1){     // outer loop 
+//             for(int j=0;j<=i;j++){  // inner loop just remember about everytime inner will be restart when i value is change
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+// Output :-
+// *
+// **
+// ***
+// ****
+// *****
+
+
+
