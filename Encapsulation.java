511462710ac7cfc42d226fc792c31e0dff3850d3
Amit <amit5yadav5762@gmail.com>
2024-01-26 21:14:59 +0530

    re revising encapsulation concept

--- Encapsulation.java
+++ Encapsulation.java
@@ -37,52 +37,53 @@
 
 
 // Next example for this keyword
-class Dog{
-    private String breed;
-    private double age;
-    private int price;
+// class Dog{
+//     private String breed;
+//     private double age;
+//     private int price;
 
-    // public void setDog(String breed,double age,int price){
-    // //   breed = breed;
-    // //   age = age; // this variable faces shadowing problem to prevent shadowing problem we have to use this keyword
-    // //    price = price;
+//     // public void setDog(String breed,double age,int price){
+//     //   breed = breed;
+//     //   age = age; // this variable faces shadowing problem to prevent shadowing problem we have to use this keyword
+//     //    price = price;
 
-    // this.breed = breed;
-    // this.age = age;
-    // this.price = price;
-    // }
+//     // // this.breed = breed;
+//     // // this.age = age;
+//     // // this.price = price;
+//     // }
 
 
-    // or we can use special setter name 'constructor '
-    public Dog(String breed, double age,int price){
-        breed = breed;
-        age = age;
-        price = price;
+//     // or we can use special setter name 'constructor '
+//     public Dog(String bree, double ge,int rice){
+//         breed = bree;
+//         age = ge;
+//         price = rice;
 
-    }
-    public String getBreed(){
-        return breed;
-    }
-    public double getAge(){
-        return age;
-    }
-    public int getPrice(){
-        return price;
-    }
-}
+//     }
+//     public String getBreed(){
+//         return breed;
+//     }
+//     public double getAge(){
+//         return age;
+//     }
+//     public int getPrice(){
+//         return price;
+//     }
+// }
 
-class Encapsulation{
-    public static void main(String[] args) {
+// class Encapsulation{
+//     public static void main(String[] args) {
 
-       Dog d = new Dog("amit",3.5,666);// this is a constructor call from starting for the core java we use constructor in java
-     //   d.Dog("AMit", 345, 245);
+//        Dog d = new Dog("amit",3.5,666);// this is a constructor call from starting for the core java we use constructor in java
+//      //   d.Dog("AMit", 345, 245);
 
-        System.out.println(d.getBreed());
-        System.out.println(d.getAge());
-        System.out.println(d.getPrice());
+//         System.out.println(d.getBreed());
+//         System.out.println(d.getAge());
+//         System.out.println(d.getPrice());
 
-    }
-}      
+//     }
+// }
+// }      
 // class dog{
 //     private int age;
 //     public void setAge(int x){
@@ -100,3 +101,20 @@ class Encapsulation{
 //        System.out.println(d.getAge()); 
 //     }
 // }
+
+// class Encapsulation{
+//     int page;
+
+//     public int getPage(){
+//         return page;
+//     }
+//     public static void main(String []args){
+//         Encapsulation gp = new Encapsulation();
+//         System.out.println(gp.getPage());
+//     }
+// }
+
+
+
+
+
