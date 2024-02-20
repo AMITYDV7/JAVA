ff5794851a737143a64f5b3e92089776b88e64da
Amit <amit5yadav5762@gmail.com>
2024-02-20 22:06:15 +0530

    finding missing number

---
+++ ArrayPractice.java
@@ -0,0 +1,35 @@
+// public class ArrayPractice {
+//     public static void main(String[] args) {
+//         int a[]= {1,2,3,4,5};
+//        int n = a.length;
+   
+// //    for(int i=0;i<n/2;i++){
+// //     if(a[i]!=a[n-i-1]){
+// //         System.out.print("NOT");
+// //     }
+// //     else{
+// //         System.out.println("hai");for
+// for(int i =n-1;i>=0;i--){
+//     System.out.print(a[i] + " ");
+// }
+//     }
+//    }
+
+
+// class ArrayPractice{
+//     public static void main(String[] args) {
+//         int a[] = {1,2,4,5,6};
+//         int sum=0;
+//         int n= a.length;
+
+//         int sum_natural = ((n+1)*(n+2))/2;
+
+//         for(int i=0;i<n;i++){
+//             sum += a[i] ;
+//         }
+    
+     
+//     int total_sum = sum_natural-sum;
+//     System.out.println(total_sum);
+//     }
+// }
--- inheritance.java
+++ inheritance.java
@@ -23,25 +23,50 @@
 
 // Example for mutli-level inheritance
 
-class Grandfather{
-    void disp(){
-        System.out.println("I am stronger");
-    }
+// class Grandfather{
+//     void disp(){
+//         System.out.println("I am stronger");
+//     }
+// }
+
+// class father extends Grandfather
+// {
+
+// }
+
+// class son extends father
+// {
+
+// }
+
+// class inheritance{
+//     public static void main(String[] args) {
+//         son s = new son();
+//         s.disp();
+//     }
+// }
+
+class parent1{
+    int a =34;
+
 }
 
-class father extends Grandfather
+class parent2{
+    int b =4;
+}
+
+class child extends parent2
 {
 
 }
 
-class son extends father
-{
 
+public class inheritance {
+public static void main(String[] args) {
+    child c = new child();
+
+    System.out.println(c.b);
+    //System.out.println(c.b);
 }
-
-class inheritance{
-    public static void main(String[] args) {
-        son s = new son();
-        s.disp();
-    }
+    
 }
